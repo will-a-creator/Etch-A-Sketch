b07d8753167046092628fa3af2b2e118100df59e
will-a-creator <williamwu896@gmail.com>
2021-05-13 19:28:46 -0400

    random color

--- file.js
+++ file.js
@@ -11,26 +11,45 @@ function createGrid(){
     }
     
 }
+let color = 'black';
 //paint after pressing the button
 var etch = function(){
     var grid = document.getElementsByClassName('box');
     var grid2 = document.getElementById('row');
 
     Array.from(grid).forEach(v => v.addEventListener('mouseover', function(){
-        v.style.background ='black';
+        v.style.background =color;
     
     }));
 };
-var defaultEtch = function(){
-    $('#default').on('click', function(){ 
-        screenReset();
-        etch();
-    })
+//copied from stack overflow! 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+function colorGrid(){
+    color = getRandomColor();
+    etch();
+    
 }
 
 createGrid();
 etch();
 defaultEtch();
+
+
+
+//clear (does not work)
+const buttonClear = document.querySelector('.clear');
+buttonClear.addEventListener('click', erase);
+
+function erase(){
+    createGrid();
+}
 
 /*
 const container = document.querySelector('.page');
